de28d7979638c42baff5242ea30add3d8769b227
Brian Morearty <brian@morearty.org>
2021-11-21 08:23:53 -0800

    Add a test.

--- app/controllers/test_async_controller.rb
+++ app/controllers/test_async_controller.rb
@@ -6,8 +6,10 @@ class TestAsyncController < ApplicationController
     RequestStore.store = { name: "Brian" }
     @response = Async do
       if RequestStore.store[:name] == "Brian"
+        cookies[:success] = "true"
         "Congrats, RequestStore.store[:name] is 'Brian'."
       else
+        cookies[:success] = "false"
         "RequestStore.store[:name] should be 'Brian' but is '#{RequestStore.store[:name]}'"
       end
     end.wait
--- test/controllers/test_async_controller_test.rb
+++ test/controllers/test_async_controller_test.rb
@@ -4,5 +4,6 @@ class TestAsyncControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get test_async_index_url
     assert_response :success
+    assert_equal "true", cookies[:success]
   end
 end
